db6f37f7e48990f7f766fb695943005c33f7e26d
AuthorX <134562085+ZeroEraX@users.noreply.github.com>
2023-06-03 20:01:10 -0400

    Daily Checklist Template

---
+++ Daily CheckList.py
@@ -0,0 +1,157 @@
+#
+#
+#
+#
+##### - DAILY CHECK LIST - ####
+#
+import sys
+#
+##### - MORNING - #####
+Day_Morning = input('Is it morning? ')
+print()
+#
+while True:
+	if Day_Morning.lower() == 'yes': 
+		print('Lets do a Morning Check list then')
+		print()
+	#
+	Bteeth = input('Did you brush your teeth this morning?: ')
+	print()
+	#
+	if Bteeth.lower() == 'yes':
+		print('Excelent')
+		print()
+	else:
+		print('Do you want to run around with the breath? Comeback when youre done')
+		print()
+		sys.exit()
+	#
+	Bkfast = input('Did you eat breakfast? ')
+	print()
+	#
+	if Bkfast.lower() == 'yes':
+		print('Awesome, Breakfast is important!')
+		print()
+	else:
+		print('Fasting? Otherwise Breakfast will make your whole day better! Check in after youre full!')
+		print()
+		sys.exit()
+	#
+	Clean_Rm = input('Did you organize your room? ')
+	print()
+	#
+	if Clean_Rm_.lower == 'yes':
+		print('Good! Almost done!')
+		print()
+	else:
+		print('I probably didnt either, continue on.')
+		print()
+		print('Unless...has it been more than 3 days?...Maybe its time to get to it.')
+		break
+#
+##### - AFTERNOON - #####
+# Day_ = input('')
+# print()
+# #
+# if Day_.lower() == 'yes': 
+# 	print('')
+# 	print()
+# #
+#  = input('')
+# print()
+# #
+# if .lower() == 'yes':
+# 	print('Excelent')
+# 	print()
+# else:
+# 	print('')
+# 	print()
+# #
+#  = input('')
+# print()
+# #
+# if .lower() == 'yes':
+# 	print('')
+# 	print()
+# else:
+# 	print('')
+# #
+#  = input('')
+# print()
+# if lower = 'yes':
+# 	print('')
+# 	print()
+# else:
+# 	print('')
+# 	print()
+# 	print('')
+#
+# ##### - EVENING - #####
+# Day_ = input('')
+# print()
+# #
+# if Day_.lower() == 'yes': 
+# 	print('')
+# 	print()
+# #
+#  = input('')
+# print()
+# #
+# if .lower() == 'yes':
+# 	print('Excelent')
+# 	print()
+# else:
+# 	print('')
+# 	print()
+# #
+#  = input('')
+# print()
+# #
+# if .lower() == 'yes':
+# 	print('')
+# 	print()
+# else:
+# 	print('')
+# #
+#  = input('')
+# print()
+# if lower = 'yes':
+# 	print('')
+# 	print()
+# else:
+# 	print('')
+# 	print()
+# 	print('')
+# #
+# #
+# #
+# #
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	 
+
